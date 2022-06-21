420a92920abb489e267a7f151b30185e41804954
Fernando Almeida <fernando.ralmeida@outlook.com>
2022-06-21 10:40:05 -0300

    up-status-online-atendimento

---
+++ src/Sim.UI.Web/Areas/Identity/Pages/OnlineStatus.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sim.Domain.Entity;
+using Sim.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Sim.UI.Web.Areas.Identity.Pages
+{
+    [Authorize]
+    public class OnlineStatusModel : PageModel
+    {
+        private readonly IAppServiceStatusAtendimento _statusAtendimento;
+
+        public OnlineStatusModel(IAppServiceStatusAtendimento statusAtendimento)
+        {
+            _statusAtendimento = statusAtendimento;
+        }
+
+        public void OnGet(string id, bool val)
+        {
+            if (!string.IsNullOrEmpty(id))
+            {
+                var gid = new Guid(id);
+
+                var meustatus = _statusAtendimento.GetIdAsync(gid).Result;
+                meustatus.Online = val;
+                 _statusAtendimento.UpdateAsync(meustatus).Wait();
+            }
+        }
+
+    }
+}
