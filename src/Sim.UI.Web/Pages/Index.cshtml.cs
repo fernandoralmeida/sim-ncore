0e238702baf786dda2bd1676d823d35fb62deaa4
Fernando Almeida <fernando.ralmeida@outlook.com>
2022-07-14 10:33:58 -0300

    up-layout-css-planner-startpage

--- src/Sim.UI.Web/Pages/Index.cshtml.cs
+++ src/Sim.UI.Web/Pages/Index.cshtml.cs
@@ -12,6 +12,9 @@ namespace Sim.UI.Web.Pages
     [Authorize]
     public class IndexModel : PageModel
     {
+
+        [TempData]
+        public string StatusMessage{get;set;}
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
