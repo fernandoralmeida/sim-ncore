79e5e727cf9886e50a315fdf7c8f750a1a662bc2
fernandoralmeida <fernando.ralmeida@outlook.com>
2022-12-14 15:46:38 -0300

    up-sebrae-reports

--- src/Sim.Application/Sebrae/Services/AppServiceSebrae.cs
+++ src/Sim.Application/Sebrae/Services/AppServiceSebrae.cs
@@ -38,8 +38,8 @@ public class AppServiceSebrae : IAppServiceSebrae
 
             var _meis = new List<KeyValuePair<string, int>>();
             _meis.Add(new KeyValuePair<string, int>("Formalização-MEI", at.Where(s => s.Servicos.Contains("Formalização-MEI")).Count()));
-            _meis.Add(new KeyValuePair<string, int>("Formalização-MEI", at.Where(s => s.Servicos.Contains("Alteração-MEI")).Count()));
-            _meis.Add(new KeyValuePair<string, int>("Formalização-MEI", at.Where(s => s.Servicos.Contains("Baixa-MEI")).Count()));
+            _meis.Add(new KeyValuePair<string, int>("Alteração-MEI", at.Where(s => s.Servicos.Contains("Alteração-MEI")).Count()));
+            _meis.Add(new KeyValuePair<string, int>("Baixa-MEI", at.Where(s => s.Servicos.Contains("Baixa-MEI")).Count()));
             _report.MEIs = _meis;
 
             var _c = new List<KeyValuePair<string, int>>();
@@ -48,6 +48,55 @@ public class AppServiceSebrae : IAppServiceSebrae
             _c.Add(new KeyValuePair<string, int>("Anônimo",at.Where(s => s.Pessoa == null && s.Empresa == null).Count()));
             _report.PerfilCliente = _c;
 
+            var _faixa_etaria = new List<string>();
+             var _list_faixa = new List<KeyValuePair<string, int>>();
+            var _genero = new List<string>();
+            var _list_genero = new List<KeyValuePair<string, int>>();
+            
+            foreach (var i in at.Where(s => s.Pessoa != null)) {
+
+                _genero.Add(i.Pessoa.Genero);
+                
+                var d1 = new DateTime(i.Pessoa.Data_Nascimento.Value.Year,
+                                        i.Pessoa.Data_Nascimento.Value.Month,
+                                        i.Pessoa.Data_Nascimento.Value.Day);
+
+                var d2 = new DateTime(i.Data.Value.Year,
+                                        i.Data.Value.Month,
+                                        i.Data.Value.Day);
+
+                var _faixa = (d2.Subtract(d1).TotalDays) / 365;
+                if (_faixa > 15 && _faixa < 21)
+                    _faixa_etaria.Add("16 -> 20 anos");
+                else if (_faixa > 20 && _faixa < 31)
+                        _faixa_etaria.Add("21 -> 30 anos");
+                else if (_faixa > 30 && _faixa < 41)
+                    _faixa_etaria.Add("31 -> 40 anos");
+                else if (_faixa > 40 && _faixa < 51)
+                    _faixa_etaria.Add("41 -> 50 anos");
+                else if (_faixa > 50 && _faixa < 61)
+                    _faixa_etaria.Add("51 -> 60 anos");
+                else if (_faixa > 60 && _faixa < 71)
+                    _faixa_etaria.Add("61 -> 70 anos");
+                else if (_faixa > 70)
+                    _faixa_etaria.Add("71 anos ou mais");
+            }
+
+            foreach (var item in _faixa_etaria.GroupBy(g => g)
+                                                .OrderByDescending(o => o.Count())) {
+                _list_faixa.Add(new KeyValuePair<string, int>(item.Key, item.Count()));
+            }
+            _report.FaixaEtariaCliente = _list_faixa;
+
+            foreach (var item in _genero.GroupBy(g => g)
+                                                .OrderByDescending(o => o.Count())) {
+                _list_genero.Add(new KeyValuePair<string, int>(item.Key, item.Count()));
+            }
+            _report.GeneroCliente = _list_genero;
+
+            _report.ClienteNovo = new KeyValuePair<string, int>("Clientes Novos", at
+                                    .Where(s => s.Pessoa != null & s.Pessoa.Data_Cadastro.Value.Year == DateTime.Now.Year)
+                                    .Count());
             
             return _report;
         });
--- src/Sim.UI.Web/Pages/SebraeAqui/Reports/Index.cshtml.cs
+++ src/Sim.UI.Web/Pages/SebraeAqui/Reports/Index.cshtml.cs
@@ -1,16 +1,44 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sim.Application.Interfaces;
+using Sim.Application.Sebrae.Interfaces;
+using Sim.Domain.Sebrae.Model;
 
 namespace Sim.UI.Web.Pages.SebraeAqui.Reports;
 
-[Authorize(Roles = "Administrador")]
+[Authorize(Roles = "Administrador,M_Sebrae,M_SebraeAdmin")]
 public class IndexModel : PageModel
 {
+    private readonly IAppServiceEvento _appevento;
+    private readonly IAppServiceSebrae _appsebrae;
+
+    [BindProperty(SupportsGet = true)]
+    public EReports LReports { get; set; }
+
     [BindProperty]
-    public string Search { get; set;}
-    public void OnGet()
+    public int InputAno { get; set; }
+
+    [TempData]
+    public string StatusMessage { get; set; }
+
+    public IndexModel(IAppServiceEvento appevento,
+                    IAppServiceSebrae appservicesebrae)  {
+        _appevento = appevento; 
+        _appsebrae = appservicesebrae;   
+    }
+    public async Task OnGetAsync()
     {
+        StatusMessage = "";
+        InputAno = DateTime.Today.Year;
+        var _list = await _appevento.DoListAsync(s => s.Data.Value.Year == InputAno);
+        LReports = await _appevento.DoBIEventosAsync(_list);
+    }
+
+    public async Task OnPostAsync()
+    {
+        var _list = await _appevento.DoListAsync(s => s.Data.Value.Year == InputAno);
+        LReports = await _appevento.DoBIEventosAsync(_list);
     }
 }
 
